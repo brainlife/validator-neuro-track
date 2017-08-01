64b1b212023cb97bba5c4672cf50df0cdbbf8e19
Soichi Hayashi <hayashis@iu.edu>
2017-08-01 14:32:18 +0000

    updated run.py to parse the header properly

--- run.py
+++ run.py
@@ -7,45 +7,39 @@ import subprocess
 import nibabel as nib
 import codecs
 
-# Things that this script checks
-#
-# * make sure tck has streamlines
-
-
 with open('config.json') as config_json:
     config = json.load(config_json)
 
-results = {"output": "", "errors": [], "warnings": []}
-
-#TODO - how should I keep up wit this path?
-mrinfo="/N/soft/rhel6/mrtrix/0.3.15/mrtrix3/release/bin/mrinfo"
+results = {"headers": {}, "errors": [], "warnings": []}
 
 directions = None
 
+#track = codecs.open(config, "r",encoding='utf-8', errors='ignore')
+with open(config['track']) as f:
+    linenum=-1
+    for line in f:
+        line = line.strip()
+        linenum+=1
 
-def validate_tck(config):
-    # TODO - Not sure how to even validate tck files
+        #reached end?
+        if line == "END":
+            break
 
-    """
-    print("running mrinfo")
-    info = subprocess.check_output(["mrinfo", config], shell=False)
-    results['t1_mrinfo'] = info  # deprecated
-    results['mrinfo'] = info
-    """
-    config = str(config)
-    track = codecs.open(config, "r",encoding='utf-8', errors='ignore')
-    ls = track.readlines()[0:4]
+        #first line should be "mrtrix tracks"
+        if linenum == 0:
+            if line != "mrtrix tracks":
+                results['errors'].append('not mrtrix tracks')
+            else:
+                continue
 
-    if int(ls[1].split()[1]) == 0:
-        results['errors'].append('tck file has no streamlines!')
+        #print linenum, line
+        tokens = line.split(":")
+        k=tokens[0].strip()
+        v=tokens[1].strip()
+        results['headers'][k] = v
 
-    s = ls[0].rstrip()
-    for line in ls[1:4]:
-        s += line.rstrip()
-
-    results['output'] = s
-
-validate_tck(config['track'])
+        if k == "count" and v == "0":
+            results['errors'].append('tck file has no streamlines!')
 
 with open("products.json", "w") as fp:
     json.dump([results], fp)
